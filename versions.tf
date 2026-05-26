0821ee7df409448c46e12e09967153d3941e41a0
Bernedotcom2312 <Bernedotcom2312@users.noreply.github.com>
2026-05-26 12:38:00 +0200

    chore(deps): bump gke and terraform versions

--- versions.tf
+++ versions.tf
@@ -5,10 +5,10 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.74.0"
+      version = "7.33.0"
     }
   }
 
-  required_version = ">= 0.14"
+  required_version = ">= 1.15"
 }
 
